c520f3aa6ba083f76f55e5ad6d59da01111886b0
DavidWells <hello@davidwells.io>
2018-07-25 14:18:32 -0700

    LS dirs

--- test-cache.js
+++ test-cache.js
@@ -1,14 +1,17 @@
 const path = require('path')
 const os = require('os')
+const child = require('child_process')
 const getSize = require('./getFolderSize')
 
 const cacheFolder = path.join(os.homedir(), '/cache')
 
-getSize(cacheFolder, (err, size) => {
-  if (err) {
-    throw err
-  }
-  console.log('Cache size')
-  console.log(size + ' bytes')
-  console.log((size / 1024 / 1024).toFixed(2) + ' MB')
-})
+child.execSync('ls', {stdio: [0, 1, 2]})
+
+// getSize(cacheFolder, (err, size) => {
+//   if (err) {
+//     throw err
+//   }
+//   console.log('Cache size')
+//   console.log(size + ' bytes')
+//   console.log((size / 1024 / 1024).toFixed(2) + ' MB')
+// })
