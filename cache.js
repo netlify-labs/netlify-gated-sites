9117120b0c27bb2624e328f5021a842321cc6481
DavidWells <hello@davidwells.io>
2018-08-14 19:25:22 -0700

    try new cache

--- cache.js
+++ cache.js
@@ -7,64 +7,56 @@ const osDir = path.join(os.homedir(), '/cache')
 console.log('OS dir', osDir)
 // const cacheFolder = path.resolve('./opt/buildhome/cache')
 // const cacheFolder = path.resolve('./cache')
-const cacheFolder = path.join('/', 'opt', 'build', 'cache', 'fast-cache')
+// Netlify cache folder
+const yourCustomNameSpace = 'fast-cache'
+const cacheFolder = path.join('/opt/build/cache', yourCustomNameSpace)
+//const cacheFolder = path.resolve('./cache')
 console.log('cacheFolder', cacheFolder)
 
 console.log('Whats in my cache before?')
 console.log('------------------')
-child.execSync('ls ../cache', {stdio: [0, 1, 2]})
+// child.execSync('ls ../cache', {stdio: [0, 1, 2]})
 
-// Do it
-const options = {
-  // directorys or files to cache
-  // contents: path.join(__dirname, 'two'),
-  contents: [
-    {
-      path: path.join(__dirname, 'node_modules'),
-      // TODO finish Cache Invalidator
-      invalidateOn: path.join(__dirname, 'package.json'),
-      command: 'npm ci'
+const oktaModulesDir = path.join(__dirname, 'okta/login-site-okta')
+
+const contentsToCache = [
+  {
+    // Directory of files to cache
+    contents: path.join(__dirname, 'node_modules'),
+    // Command or Function to run on `shouldCacheUpdate = true`
+    handleCacheUpdate: 'npm ci',
+    // Should cache update? Return true or false
+    shouldCacheUpdate: async (data, utils) => {
+      const nodeModulesChanged = await utils.diff(path.join(__dirname, 'package.json'))
+      return nodeModulesChanged
     },
-    // {
-    //   path: path.join(__dirname, 'okta/gated-site-1/node_modules'),
-    //   // TODO finish Cache Invalidator
-    //   invalidateOn: path.join(__dirname, 'okta/gated-site-1/package.json'),
-    //   command: 'npm install'
-    // },
-    // {
-    //   path: path.join(__dirname, 'okta/gated-site-2/node_modules'),
-    //   // TODO finish Cache Invalidator
-    //   invalidateOn: path.join(__dirname, 'okta/gated-site-2/package.json'),
-    //   command: 'npm install'
-    // },
-    {
-      path: path.join(__dirname, 'okta/login-site-okta/node_modules'),
-      // TODO finish Cache Invalidator
-      invalidateOn: path.join(__dirname, 'okta/login-site-okta/package.json'),
-      command: 'npm ci'
-    }
-  ],
-  // cache folder destination
-  cacheFolder: cacheFolder,
-
-  ignoreIfFolderExists: false,
-  // TODO finish Cache Invalidator
-  invalidateOn: () => {
-    return ''
+  },
+  {
+    contents: path.join(oktaModulesDir, 'node_modules'),
+    // Command or Function to run on `shouldCacheUpdate = true`
+    handleCacheUpdate: 'npm ci',
+    // Should cache update? Return true or false
+    shouldCacheUpdate: async (data, utils) => {
+      const nodeModulesChanged = await utils.diff(path.join(oktaModulesDir, 'package.json'))
+      return nodeModulesChanged
+    },
   }
-}
+]
 
-cacheMagic(options).then(() => {
-  console.log('Success! You are ready to rock. Caches saved')
-  getSize(cacheFolder, (err, size) => {
-    if (err) {
-      throw err
-    }
-    console.log('Cache size')
-    console.log(size + ' bytes')
-    console.log((size / 1024 / 1024).toFixed(2) + ' MB')
-    console.log('Whats in my cache After?')
-    console.log('------------------')
-    child.execSync('find ../cache/fast-cache -type d -print -maxdepth 6', {stdio: [0, 1, 2]})
+cacheMagic(cacheFolder, contentsToCache).then((cacheInfo) => {
+  console.log('Success! You are ready to rock')
+  cacheInfo.forEach((info) => {
+    console.log(info.cacheDir)
   })
+  // getSize(cacheFolder, (err, size) => {
+  //   if (err) {
+  //     throw err
+  //   }
+  //   console.log('Cache size')
+  //   console.log(size + ' bytes')
+  //   console.log((size / 1024 / 1024).toFixed(2) + ' MB')
+  //   console.log('Whats in my cache After?')
+  //   console.log('------------------')
+  //   child.execSync('find ../cache/fast-cache -type d -print -maxdepth 6', {stdio: [0, 1, 2]})
+  // })
 })
